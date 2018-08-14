e3c02407bff2c9a6e6fe693d7a3749b71f37c56b
Corinne Mullan <71374@ACDM.DS.SAIT.CA>
2018-08-14 16:32:31 -0600

    Initial commit

---
+++ CPRG214_Lab1/default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="CPRG214_Lab1._default" %>
+
+<!DOCTYPE html>
+
+<!-- 
+     CPRG214 Lab1
+     Author:  Corinne Mullan
+     Date:    July 19, 2018     
+-->
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="css/style.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Temperature Conversion</h1>
+            <img alt="thermometer image" class="auto-style1" src="img/thermometer.png" /><br />
+            <asp:TextBox ID="txtFromTemperature" class="control" runat="server"></asp:TextBox>
+            <asp:DropDownList ID="ddlFromUnits" class="control" runat="server">
+                <asp:ListItem Selected="True">Celsius</asp:ListItem>
+                <asp:ListItem>Fahrenheit</asp:ListItem>
+                <asp:ListItem>Kelvin</asp:ListItem>
+            </asp:DropDownList>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" class="validator" runat="server" ControlToValidate="txtFromTemperature" Display="Dynamic" ErrorMessage="Enter a temperature value"></asp:RequiredFieldValidator>
+            <asp:RangeValidator ID="RangeValidator1" runat="server" class="validator" ControlToValidate="txtFromTemperature" Display="Dynamic" ErrorMessage="Enter a value between -1000 and 1000" MaximumValue="1000" MinimumValue="-1000" Type="Double"></asp:RangeValidator>
+            <br />
+            <asp:TextBox ID="txtToTemperature" class="control readonly" runat="server" ReadOnly="True"></asp:TextBox>
+            <asp:DropDownList ID="ddlToUnits" class="control" runat="server">
+                <asp:ListItem>Celsius</asp:ListItem>
+                <asp:ListItem Selected="True">Fahrenheit</asp:ListItem>
+                <asp:ListItem>Kelvin</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <asp:Button ID="btnConvert" class="button" runat="server" OnClick="btnConvert_Click" Text="Convert" />
+            <asp:Button ID="btnClear" class="button" runat="server" CausesValidation="False" OnClick="btnClear_Click" Text="Clear" />
+        </div>
+        <asp:Label ID="lblError" class="error" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ CPRG214_Lab1/default.aspx.cs
@@ -0,0 +1,113 @@
+﻿/*
+ * CPGR214 Lab1
+ * Author:  Corinne Mullan
+ * Date:    July 19, 2018
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CPRG214_Lab1
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConvert_Click(object sender, EventArgs e)
+        {
+            // Local variables
+            double tempFrom;        // The temperature value to convert from
+            double tempTo = 0;      // The converted temperature
+
+            string unitsFrom;       // The units to convert from
+            string unitsTo;         // The units to convert to
+
+            // Obtain the input values
+            tempFrom = Convert.ToDouble(txtFromTemperature.Text);
+
+            unitsFrom = ddlFromUnits.SelectedValue;
+            unitsTo = ddlToUnits.SelectedValue;
+
+            // First check the value is not below absolute zero
+            if ((unitsFrom == "Celsius" && tempFrom < -273.15) ||
+                (unitsFrom == "Fahrenheit" && tempFrom < -459.67) ||
+                (unitsFrom == "Kelvin" && tempFrom < 0.0))
+            {
+                lblError.Text = "Error: temperature cannot be less than absolute zero";
+                txtToTemperature.Text = "";
+            }
+            else
+            // If the input value is good, perform the calculation
+            {
+                lblError.Text = "";
+
+                // If the unitsFrom and unitsTo are the same, tempTo = tempFrom
+                if (unitsFrom == unitsTo)
+                {
+                    tempTo = tempFrom;
+                }
+                // Else perform the required conversion calculation
+                else if (unitsFrom == "Celsius")
+                {
+                    if (unitsTo == "Fahrenheit")
+                    {
+                        // Convert from Celcius to Fahrenheit
+                        tempTo = 9.0 / 5.0 * tempFrom + 32.0;
+                    }
+                    else if (unitsTo == "Kelvin")
+                    {
+                        // Convert from Celcius to Kelvin
+                        tempTo = tempFrom + 273.15;
+                    }
+                }
+                else if (unitsFrom == "Fahrenheit")
+                {
+                    if (unitsTo == "Celsius")
+                    {
+                        // Convert from Fahrenheit to Celsius
+                        tempTo = (tempFrom - 32.0) * 5.0 / 9.0;
+                    }
+                    else if (unitsTo == "Kelvin")
+                    {
+                        // Convert from Fahrenheit to Kelvin
+                        tempTo = (tempFrom - 32.0) * 5.0 / 9.0 + 273.15;
+                    }
+                }
+                else if (unitsFrom == "Kelvin")
+                {
+                    if (unitsTo == "Celsius")
+                    {
+                        // Convert from Kelvin to Celsius
+                        tempTo = tempFrom - 273.15;
+
+                    }
+                    else if (unitsTo == "Fahrenheit")
+                    {
+                        // Convert from Kelvin to Fahrenheit
+                        tempTo = 9.0 / 5.0 * (tempFrom - 273.15) + 32.0;
+                    }
+                }
+
+                // Output the converted value, rounding to at most one decimal place
+                txtToTemperature.Text = tempTo.ToString("0.##");
+            }
+        }
+
+        // Reset the controls when the clear button is clicked
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFromTemperature.Text = "";
+            txtToTemperature.Text = "";
+            ddlFromUnits.SelectedValue = "Celsius";
+            ddlToUnits.SelectedValue = "Fahrenheit";
+            lblError.Text = "";
+        }
+    }
+}
